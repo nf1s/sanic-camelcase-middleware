1aa373531a0a73831fd5f6cdeacb20967afee854
ahmednafies <ahmed.nafies@gmail.com>
2020-02-24 15:53:32 +0100

    adds docsting

--- sanic_camelcase_middleware/__init__.py
+++ sanic_camelcase_middleware/__init__.py
@@ -17,11 +17,22 @@ class Camelize:
     def __init__(self, app):
         @app.middleware("request")
         async def request_to_snake_case(request):
+            """Function converts request body to snake_case
+            
+            Args:
+                request: Sanic HttpRequest Object
+            """
             if request.body:
                 request.body = bytes(dumps(decamelize(loads(request.body))), "utf-8")
 
         @app.middleware("response")
         async def response_to_camel(request, response):
+            """Function converts response body to camelcase
+            
+            Args:
+                request (HttpRequest): Sanic HttpRequest Object
+                response (HttpRequest): Sanic HttpResponse Object
+            """
             if response.body:
                 response.body = bytes(dumps(camelize(loads(response.body))), "utf-8")
 
