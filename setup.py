f885bd0a6200fd648b6c35050990ea2090db2ea0
ahmednafies <ahmed.nafies@gmail.com>
2020-03-06 08:55:55 +0100

    fixes pypi documentation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.md") as f:
     long_description = f.read()
 
-VERSION = "1.1.2"
+VERSION = "1.1.3"
 
 setup(
     name="sanic_camelcase_middleware",
@@ -12,11 +12,16 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://ahmednafies.github.io/sanic_camelcase_middleware/",
+    project_urls={
+        "Documentation": "https://ahmednafies.github.io/sanic_camelcase_middleware/",
+        "Source": "https://github.com/ahmednafies/sanic_camelcase_middleware",
+    },
     author="Ahmed Nafies",
     author_email="ahmed.nafies@gmail.com",
     license="MIT",
     packages=["sanic_camelcase_middleware"],
     install_requires=["sanic", "pyhumps"],
+    python_requires=">=3.6",
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
